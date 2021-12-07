265af8978cbd31834b35bf66fab0f82894f6fe58
YenHsinWu <kelly850408@gmail.com>
2021-12-07 20:32:59 +0800

    Add Vertex class constructors and destructor.

---
+++ include/Vertex.h
@@ -0,0 +1,28 @@
+#ifndef __VERTEX_H__
+#define __VERTEX_H__
+
+#include <iostream>
+#include <string>
+
+namespace BioAlign{
+    //////////////////
+    // Vertex class //
+    //////////////////
+
+    class Vertex{
+        private:
+            std::string m_name;
+            double m_weight;
+            Vertex *m_left;
+            Vertex *m_right;
+
+        public:
+            Vertex();
+            Vertex(std::string, double);
+            Vertex(Vertex*, Vertex*, double);
+            Vertex(const Vertex&);
+            ~Vertex();
+    };
+};
+
+#endif
---
+++ src/Vertex.cpp
@@ -0,0 +1,39 @@
+#include "Vertex.h"
+
+namespace BioAlign{
+    //////////////////
+    // Vertex class //
+    //////////////////
+
+    Vertex::Vertex():m_weight(0){
+        m_name.clear();
+        m_left = nullptr;
+        m_right = nullptr;
+    }
+
+    Vertex::Vertex(std::string name, double w):m_weight(w){
+        m_name = name;
+        m_left = nullptr;
+        m_right = nullptr;
+    }
+
+    Vertex::Vertex(Vertex *left, Vertex *right, double w):m_weight(w){
+        m_name = '(' + left->m_name + ',' + right->m_name + ')';
+        m_left = left;
+        m_right = right;
+    }
+
+    Vertex::Vertex(const Vertex& vtx){
+        m_name = vtx.m_name;
+        m_weight = vtx.m_weight;
+        m_left = vtx.m_left;
+        m_right = vtx.m_right;
+    }
+
+    Vertex::~Vertex(){
+        m_name.clear();
+        m_weight = 0;
+        m_left = nullptr;
+        m_right = nullptr;
+    }
+};
